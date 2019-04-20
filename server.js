de7d79893c70285a576a7f285a3e70a1b4d30b85
Ken Hall <kenhallthe3rd@gmail.com>
2019-04-20 13:58:56 -0700

    fix on bork plotting skeleton

--- server.js
+++ server.js
@@ -8,9 +8,11 @@ app.use(express.static('public'));
 
 const login = require('./routes/login.js');
 const user_page = require('./routes/user_page.js');
+const plotlyTest = require('./routes/plotlyTest.js');
 
 app.use('/login', login);
 app.use('/user_page', user_page);
+app.use('/plotlyTest', plotlyTest);
 
 app.listen(12223, () => {
     console.log("Server started");
