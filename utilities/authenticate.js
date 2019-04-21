641e7004915af15152d136a035df5aeec21d9cea
Ken Hall <kenhallthe3rd@gmail.com>
2019-04-21 15:35:01 -0700

    implemented barebones authentication model

--- utilities/authenticate.js
+++ utilities/authenticate.js
@@ -1,6 +1,22 @@
+authenticate = {};
 
-function authenticate(username, password, database_or_array_or_table_or_whatever_is_used_for_checking){
-
+authenticate.isAuth = (req, res) => {
+    console.log(req.session);
+    if( req.session.isAuthenticated === undefined ){
+        res.redirect('/login');
+        return 0;
+    } else {
+        return 1;
+    }
 }
 
+authenticate.checkCredentials = (req,res) => {
+    // check for username + password in the database
+
+    // if username + password not in database, send back to login with error message
+
+    // if in database, reroute appropriately based on whether is user/admin
+
+    // set req.session.isAuthenticated to true
+}
 module.exports = authenticate;
