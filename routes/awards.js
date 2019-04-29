046f63b87b30c9ec856bdcb156150e3ced49658f
Ken Hall <kenhallthe3rd@gmail.com>
2019-04-28 20:00:16 -0700

    database queriable

--- routes/awards.js
+++ routes/awards.js
@@ -3,43 +3,53 @@ var router = express.Router();
 const sqlite3 = require('sqlite3').verbose();
 const fs = require('fs');
 
-// database check/initialization
 try{
-    var databaseChecker = fs.readFileSync('./database/awards.db');
-} catch(err) {
-    // unable to open database file; initialize database
-    var db = openDatabase();
+    let db = openDatabase();
     db.serialize( () => {
-        db.run("CREATE TABLE awards (award_type Int, awardee_name Varchar, awardee_email Varchar, awarder_ID Int, timestamp Int");
-        db.run("INSERT INTO awards VALUES (1, Ken, kenhallthe3rd@gmail.com, 4, 958754");
+        //db.run("CREATE TABLE awards(award_type Int, awardee_name text, awardee_email text, awarder_ID Int, timestamp Int)");
+        db.run(`INSERT INTO awards (award_type, awardee_name, awardee_email, awarder_ID, timestamp) VALUES (?, ?, ?, ?, ?)`, [1, 'Ken', 'kenhallthe3rd@gmail.com', 4, 123456], (err) => {
+            if(err){
+                console.error(err);
+            }
+        });
     })
     db.close( (err) => {
         if(err){
             console.error(err);
         }
     })
+} catch(err) {
+    console.error(err);
 }
 
 router.get('/searchByEmail/:email', (req, res) => {
-    var email = req.param['email'];
+    var email = req.params['email'];
+    console.log(email);
     var db = openDatabase();
+    var results = []
     db.serialize( () => {
         db.all("SELECT * FROM awards WHERE awardee_email = ?", [email], (err, matches) => {
             for( match in matches ){
                 console.log(matches[match]);
+                results.push(matches[match]);
             }
+            res.send(results);
         });
+    })
+    db.close( (err) => {
+        if(err){
+            console.error(err);
+        }
     })
 })
 
 module.exports = router;
 
 function openDatabase(){
-    var db = new sqlite3.Database('./database/awards.db', sqlite3.OPEN_CREATE, (err) => {
+    let db = new sqlite3.Database('./database/awards.db', sqlite3.OPEN_READWRITE, (err) => {
         if(err){
             console.error(err);
         }
-        console.log("Opened to fs-based database");
     });
     return db;
 }
--- server.js
+++ server.js
@@ -60,6 +60,8 @@ const emailTest = require('./routes/emailTest');
 app.use('/emailTest', emailTest);
 const plotlyTest = require('./routes/plotlyTest');
 app.use('/plotlyTest', plotlyTest);
+const awards = require('./routes/awards');
+app.use('/awards', awards);
 
 passport.use('local_user',new LocalStrategy(
     {
