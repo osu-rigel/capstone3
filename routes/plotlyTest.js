c1008ffafa06fcaa013f5be678947d7c3f6eb567
Ken Hall <kenhallthe3rd@gmail.com>
2019-05-05 10:03:10 -0700

    little bit of progress made

--- routes/plotlyTest.js
+++ routes/plotlyTest.js
@@ -1,12 +1,37 @@
 var express = require('express');
 var router = express.Router();
 const auth = require('../utilities/authenticate.js');
+const db = require ('../db.js');
 
 router.get('/', (req, res) => {
-    if( auth.isLoggedIn(req,res) === 0 ){
+    /*if( auth.isLoggedIn(req,res) === 0 ){
         return;
-    }
+    }*/
     res.render('plotlyTest');
+})
+
+router.get('/awardsGiven', (req, res) => {
+    /*if( auth.isLoggedIn(req,res) === 0 ){
+        return;
+    }*/
+
+})
+
+router.get('/awardsReceived', (req, res) => {
+    /*if( auth.isLoggedIn(req,res) === 0 ){
+        return;
+    }*/
+    var SQLparams = [];
+    var SQLquery = "";
+    if( req.query['field'] === undefined ){
+        SQLquery = "SELECT * FROM awards";
+    }
+    db.query(SQLquery, SQLparams, (err, result) => {
+        // parse the results
+        
+        res.send(JSON.stringify());
+    });
+
 })
 
 
