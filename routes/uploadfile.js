4fd6475b1d6f20286cb8af0791f547cff8f2dcb4
prashara <46575311+prashara@users.noreply.github.com>
2019-05-05 10:11:03 -0700

    Update uploadfile.js

--- routes/uploadfile.js
+++ routes/uploadfile.js
@@ -1,26 +1,46 @@
 var express = require('express');
 var router = express.Router();
+
 const auth = require('../utilities/authenticate.js');
 
-
 router.get('/', (req, res) => {
-    if( auth.isLoggedIn(req,res) === 0 ){
-        return;
-    }
     res.render('fileupload');
 });
 
 router.post('/', (req, res) => {
-    if( auth.isLoggedIn(req,res) === 0 ){
-        return;
-    }
     global.upload(req,res,function(err){
         if(err){
             res.render('fileupload',{msg:err});
             console.log(err);
         }else{
+            const db = require ('../db.js');           // To go down one directory we use .. here.
             console.log(req.file);
-            res.send('test');
+            
+            //res.send('test');
+            console.log('file received');
+            //console.log(req);
+            
+            db.query('Insert INTO file (name,type, size) VALUES (?,?,?)',[req.file.filename,req.file.mimetype,req.file.size],function(error,results,fields){  
+                if(error) throw error;
+                console.log("Inserted data");
+                
+                // db.query('SELECT LAST_INSERT_ID(), name from file',function(error,results,fields){
+                //     if(err) throw error;
+                    
+                //     console.log("Results are: ");
+                //     console.log(results[0].name);
+                //     imgName = results[0].name;
+                // });
+               
+            });
+            var message = "Successfully! uploaded";
+            console.log("Image name here as well");
+            var imgName = req.file.filename;
+            console.log(imgName);
+            res.render('image',{message: message,imgName: imgName});
+            
+            
+            
         }
         
     });
