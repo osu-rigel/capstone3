043ccb061f3123ee672566295adb2b8892eb2a52
Ken Hall <kenhallthe3rd@gmail.com>
2019-05-14 14:35:15 -0700

    Emailer now makes PDF with LaTex to send as attach

--- routes/emailTest.js
+++ routes/emailTest.js
@@ -2,13 +2,24 @@ var express = require('express');
 var router = express.Router();
 const emailer = require('../utilities/emailer.js');
 const auth = require('../utilities/authenticate.js');
+const latex = require('node-latex');
+const fs = require('fs');
 
 router.get('/', (req, res) => {
     if( auth.isLoggedIn(req,res) === 0 ){
         return;
     }
-    emailer("hallkenn@oregonstate.edu, prashara@oregonstate.edu, mistrya@oregonstate.edu", "CAPSTONE EMAIL TEST", "<h1>HELLO WORLD</h1>", './database/test_attach.jpg');
-    res.sendStatus(200);
+    var input = fs.readFileSync("./utilities/input.tex", "utf8");
+    input = input.replace('AWARD', 'Employee of the Month');
+    input = input.replace('DATE', 'RIGHT NOW');
+    input = input.replace('SIGNATURE', 'KENNETH W HALL III');
+    var pdf = latex(input);
+    var output = fs.createWriteStream('./utilities/PDFs/output.pdf');
+    pdf.pipe(output);
+    pdf.on('finish', () => {
+        emailer("hallkenn@oregonstate.edu", "CAPSTONE EMAIL TEST", "<h1>HELLO WORLD</h1>", './utilities/PDFs/output.pdf');
+        res.sendStatus(200);    
+    })
 })
 
 
